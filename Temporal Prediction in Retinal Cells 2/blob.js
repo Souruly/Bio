aeeeae65245aa800b41152790d45a98eb9265058
Escher <souruly@gmail.com>
2018-12-28 19:35:49 +0530

    Better Visual Effect

--- Temporal Prediction in Retinal Cells 2/blob.js
+++ Temporal Prediction in Retinal Cells 2/blob.js
@@ -9,16 +9,14 @@ function Blob(pos, r)
     noStroke();
     if(this.alive)
     {
-      fill(255,51,255,16);
-      ellipse(this.position.x, this.position.y, this.r*1.6, this.r*1.6);
-      fill(255,51,255,32);
-      ellipse(this.position.x, this.position.y, this.r*1.4, this.r*1.4);
-      fill(255,51,255,64);
-      ellipse(this.position.x, this.position.y, this.r*1.2, this.r*1.2);
-      fill(255,51,255,128);
-      ellipse(this.position.x, this.position.y, this.r, this.r);
-      // fill(255,51,255,192);
-      // ellipse(this.position.x, this.position.y, this.r*0.8, this.r*0.8);
+      let lim = 10;
+      for(let i=0 ; i<lim ; i++)
+      {
+        let alpha = map(i,0,lim,8,80)
+        fill(255,51,255,alpha);
+        let rMultpiplier = map(i,0,4,1.5,0.9);
+        ellipse(this.position.x, this.position.y, this.r*rMultpiplier, this.r*rMultpiplier);
+      }
     }
     else
     {
